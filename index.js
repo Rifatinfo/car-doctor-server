fce780b4c42f350cf1ff8cf622016cb3a12b094a
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-09-29 19:47:14 +0600

    server added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 //middleware
@@ -29,13 +29,34 @@ async function run() {
     await client.connect();
 
     const serviceCollection = client.db("carDoctor").collection("services");
+    const bookingCollection = client.db("carDoctor").collection("bookings");
     
     app.get('/services', async(req, res) => {
         const cursor = serviceCollection.find();
         const result = await cursor.toArray();
         res.send(result);
     })
+    
+    app.get('/services/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const options = {
+            // Sort matched documents in descending order by rating
+            sort: { "imdb.rating": -1 },
+            // Include only the `title` and `imdb` fields in the returned document
+            projection: {  title: 1, price: 1 , service_id: 1},
+          };
+        const result = await serviceCollection.findOne(query, options);
+        res.send(result);
+    })
 
+    // bookings 
+    app.post('/bookings', async( req, res) => {
+        const booking = req.body;
+        console.log(booking);
+        const result = await bookingCollection.insertOne(booking);
+        res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
